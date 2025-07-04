c4e73a0138f849c3178964d0bd2439361bfa6434
letu05 <148754162+letu05@users.noreply.github.com>
2025-07-04 21:55:26 +0700

    add
    add

---
+++ Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// tính dien tich hinh tron
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập bán kính hình tròn");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double s = r * r * Math.PI;
+        Console.WriteLine("Dien tich la " + s);
+
+    }
+}
---
+++ Example3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("nhập họ và tên");
+        string hoten = Console.ReadLine();
+        Console.WriteLine("nhập tuổi");
+        int tuoi = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Ho va Ten {hoten} và tuổi là {tuoi}");
+    }
+}
