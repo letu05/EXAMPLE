5981d570c05142d847bf3079693201c4a010a644
letu05 <148754162+letu05@users.noreply.github.com>
2025-07-30 13:19:45 +0700

    ADD
    ADD

---
+++ Baitapc#/utln/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        while(a != b)
+        {
+            if (a > b)
+            {
+                a -= b;
+            }
+            else
+            {
+                b -= a;
+            }
+        }
+    }
+}
