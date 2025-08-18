8fff44c6c930962f7e183278135d2d4d4a41a3ac
letu05 <148754162+letu05@users.noreply.github.com>
2025-08-18 21:52:11 +0700

    d

---
+++ New folder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "B:\\filePath";
+        DirectoryInfo dirInfo = Directory.CreateDirectory(filePath);
+        // DirectoryInfo.Create() is not valid, removed.
+        // You can use dirInfo for further operations if needed.
+    }
+}
